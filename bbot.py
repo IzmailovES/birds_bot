37e3d4f0542f1862b8cf61e0308528a986b0b744
Egor <81759932+IzmailovES@users.noreply.github.com>
2024-01-29 16:17:34 +0300

    Create bbot.py
    main executable

---
+++ bbot.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+import birds
+
+BOT_TOKEN = ''
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+birds = birds.load_birds('dbb')
+
+@dp.message(Command(commands=['start']))
+async def process_start_command(message:Message):
+    await message.answer('Hello! This is birds bot')
+
+@dp.message(Command(commands=['list']))
+async def list_of_birds(message:Message):
+    lst = map(lambda x: x['short_name'], birds)
+    string = '\n'.join(lst)
+    await message.answer(f'list of birds:\n{string}')
+
+@dp.message(Command(commands=['list_images']))
+async def list_of_birds(message:Message):
+    await message.answer('list of birds with images')
+
+@dp.message(Command(commands=['count']))
+async def birds_count(message:Message):
+    await message.answer(f'count of birds  in base: {len(birds)}')
+
+@dp.message()
+async def default_answer(message:Message):
+    await message.answer('default answer to non-handling message')
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
